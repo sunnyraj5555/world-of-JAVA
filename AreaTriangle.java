cd362d4556759898a3357470d894ce9508c6cc22
sunnyraj5555 <headheal2206@gmail.com>
2021-01-15 13:49:25 +0530

    Declare Variable

--- AreaTriangle.java
+++ AreaTriangle.java
@@ -2,7 +2,10 @@ public class Area
 {
     	public static void main(String arg[])  
     	{
-
+		int b=Integer.parseInt(arg[0]);
+		int h=Integer.parseInt(arg[1]);
+		double r=triArea(b,h);
+		System.out.println("Area of the Triangle="+r);
 	}
 
 }
