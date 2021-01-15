1c372acc6a7227af3993a524f9f1c2a7c0c19659
sunnyraj5555 <headheal2206@gmail.com>
2021-01-15 13:56:04 +0530

    Area of Circle

--- AreaCircle.java
+++ AreaCircle.java
@@ -4,8 +4,16 @@
         // Main function
         public static void main(String arg[])
         {
+		int r=Integer.parseInt(arg[0]);
+                double a=cirArea(r);
+                System.out.println("Area of the Circle="+a);
+        }
+            //Function
+                static double cirArea(int s)
+                {
+                return (r*r); // returning area
+                }
 
 
 
-	}
 }
