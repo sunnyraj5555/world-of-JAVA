619632aa889040ed1689beee64a6a3e993f1d46b
sunnyraj5555 <headheal2206@gmail.com>
2021-01-15 13:36:08 +0530

    Initialising variables

--- AreaSquare.java
+++ AreaSquare.java
@@ -3,7 +3,10 @@ public class Area
 	// Main function
 	public static void main(String arg[])
 	{
-
+		int b=Integer.parseInt(arg[0]);
+		int h=Integer.parseInt(arg[1]);
+		double r=triArea(b,h);
+		System.out.println("Area of the Triangle="+r);
 	}
 
 
