3421f5d93910d279d2716991fdddabb069977a11
sunnyraj5555 <headheal2206@gmail.com>
2021-01-15 12:44:22 +0530

    Print table of a given number

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table
+{
+	public static void main(String arg[])
+	{
+		int i=Integer.parseInt(arg[0]);
+		for (int k=1; k<=10;k++)
+		{
+		System.out.println(i+"*"+k+"="+(i*k));
+		}
+	}
+}
