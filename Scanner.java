29c8efe5746129a7e870add61275f7c4ae4fbaa5
sunnyraj5555 <headheal2206@gmail.com>
2021-01-20 13:47:01 +0530

    Created a Scanner Object

---
+++ Scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class Scanner{
+   public static void main(String[] args){
+	Scanner myObj = new Scanner(System.in);
+      	//classname -> ref. var -> = -> new -> constructor;
+      	//new -> keyword
+	//myObj -> referance variable
+	
+	System.out.println("Enter Your name");
+
